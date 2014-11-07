3a96b8d5989682ee33325e49ced0463c87fbe841
Giampaolo Bellavite <gpberlin@gmail.com>
2014-11-07 22:56:21 +0100

    Updated example

--- SwiftChart/ViewController.swift
+++ SwiftChart/ViewController.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ChartDelegate {
 
+    
+    @IBOutlet weak var chartLabel: UILabel!
     @IBOutlet weak var chart: Chart!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        chart.xLabels = [0, 1, 2, 3, 4, 5, 6]
+        
+        chartLabel.text = " "
+        
+        let serie = ChartSerie(data: [(x: 1, y: 2), (x: 2, y: 3), (x: 3, y: 2), (x: 4, y: 4), (x: 5, y: 3), (x: 6, y: 5), (x: 7, y: 2.5), (x: 8, y: 6)])
+        chart.minY = 0
+        chart.addSerie(serie)
+        chart.delegate = self
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +31,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func didTouchInsideChart(chart: Chart, point: CGPoint, axisValues: ChartPoint, data: Array<ChartPoint?>, indexes: Array<Int?>) {
+        if let values = data[0] {
+            chartLabel.text = "x: \(axisValues.x) \(axisValues.y) | data=\(values.x), \(values.y)"
+        }
+    }
+    
+    func didTouchOutsideChart(chart: Chart) {
+        chartLabel.text = " "
+    }
 
 }
 

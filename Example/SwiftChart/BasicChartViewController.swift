15eb2d095acd16fdaaca55d67d4012965be15111
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-07-06 14:34:41 +0200

    Update example to replicate #26

--- Example/SwiftChart/BasicChartViewController.swift
+++ Example/SwiftChart/BasicChartViewController.swift
@@ -51,7 +51,7 @@ class BasicChartViewController: UIViewController, ChartDelegate {
             // Chart with y-min, y-max and y-labels formatter
             
             
-            let data: Array<Float> = [3, 6, -2, 6, 2, 4, -4, 3, -6, -1, -5]
+            let data: Array<Float> = [0, -2, -2, 3, -3, 4, 1, 0, -1]
             
             let series = ChartSeries(data)
             series.area = true

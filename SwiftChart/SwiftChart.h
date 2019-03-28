56b458573b53a4cfbd2ff284c6cd759b2753bcd0
Thomas De Leon <tdeleon@liveperson.com>
2019-03-28 13:58:42 -0700

    Add framework

---
+++ SwiftChart/SwiftChart.h
@@ -0,0 +1,19 @@
+//
+//  SwiftChart.h
+//  SwiftChart
+//
+//  Created by Thomas De Leon on 3/28/19.
+//  Copyright © 2019 LivePerson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftChart.
+FOUNDATION_EXPORT double SwiftChartVersionNumber;
+
+//! Project version string for SwiftChart.
+FOUNDATION_EXPORT const unsigned char SwiftChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftChart/PublicHeader.h>
+
+
